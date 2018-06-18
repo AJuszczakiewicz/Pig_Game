c86e5f7184efd1d26cfd30b4ba848e4b580e29a0
AJuszczakiewicz <al.jusz@gmail.com>
2018-06-19 01:09:48 +0200

    Set displayed value at beginning to 0

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ activePlayer = 1;
 
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
     let dice = Math.floor(Math.random()*6) + 1;
@@ -26,5 +30,6 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     let diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
     diceDOM.src = 'img/dice-' + dice + '.png';
+
 
 });
